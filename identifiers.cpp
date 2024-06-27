e79ac6383befed2d207e935438ed30dff2773339
muskan <muskan8878h@gmail.com>
2024-06-27 15:28:56 +0530

    identifiers

---
+++ identifiers.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+// class Car_24 { 
+// 	string Brand; 
+// 	string model; 
+// 	int year; 
+// }; 
+void calculateSum(int a, int b) 
+{ 
+	int _sum = a + b; 
+	cout << "The sum is: " << _sum << endl; 
+} 
+int main() 
+{ 
+	int studentAge = 20; 
+	double accountBalance = 1000.50; 
+	string student_Name = "Karan"; 
+	calculateSum(2, 10); 
+	return 0; 
+}
