b512d89b64fddf78e371559468f6fef79284d49a
muskan <muskan8878h@gmail.com>
2024-06-28 10:22:54 +0530

    indexing of string and updation of string

--- indexing_string.cpp
+++ indexing_string.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main(){
     string a = "My Name is Muskan";
+    
+    cout<<a[0]<<endl; //print the specific character
+    
+    a[0]='m'; //update string 
     cout<<a<<endl;
 }
