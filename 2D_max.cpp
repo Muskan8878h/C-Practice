6861576152ebd79395235ab187b0f74c0ffd677d
muskan <muskan8878h@gmail.com>
2024-06-28 09:49:23 +0530

    maximum number in 2D array

---
+++ 2D_max.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[2][2];
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+    cout<<endl;
+    int mx=arr[0][0];
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            if(arr[i][j]>mx){
+                mx=arr[i][j];
+            }
+        }
+    }
+    cout<<"maximum element is : "<<mx;
+}
