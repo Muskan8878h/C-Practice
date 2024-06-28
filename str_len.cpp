0dd447c5d4648a8f90fcfbd1a966e42837c33dc0
muskan <muskan8878h@gmail.com>
2024-06-28 10:16:53 +0530

    use of getline in string

---
+++ str_len.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string a = "My Name is Muskan";
+    cout<<a<<endl;
+}
--- string.cpp
+++ string.cpp
@@ -1,6 +1,18 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string a = "My Name is Muskan";
+//     cout<<a<<endl;
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-    string a = "My Name is Muskan";
-    cout<<a<<endl;
+    string a,b;
+    // cin>>a;  //MY name is Muskan
+    // cout<<a; //My
+
+    //if we use this
+    getline(cin,b);
+    cout<<b;
 }
