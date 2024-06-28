4480c4d79f2640de3f2d597ad719855b4c0f0dc8
muskan <muskan8878h@gmail.com>
2024-06-28 09:24:51 +0530

    reverse of an array without using another array

---
+++ reverse_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int i=0,j=n-1;
+    while(i<j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        i++;
+        j--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
