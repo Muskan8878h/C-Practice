664c36323c16d55c5fa98a378a99868a57c39d0d
muskan <muskan8878h@gmail.com>
2024-06-28 12:25:09 +0530

    sizeof operator

---
+++ sizeof.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"size of int "<<a<<endl;
+    float b;
+    cout<<"size of float "<<b<<endl;
+    char c;
+    cout<<"size of char "<<c<<endl;
+    bool d;
+    cout<<"size of bool "<<d;
+}
