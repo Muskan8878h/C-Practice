3507989975740d2c4ecc1b94a0f9e34bd4b8f4d0
muskan <muskan8878h@gmail.com>
2024-06-28 09:03:49 +0530

    reverse of an array

--- rev_arr.cpp
+++ rev_arr.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={3,6,5,4,7,8};
-    int n=sizeof(arr)/sizeof(arr[0]);
+    int arr_one[]={3,6,5,4,7,8};
+    int n=sizeof(arr_one)/sizeof(arr_one[0]);
     int j;
+    int arr_two[n];
     for (int i=0;i<n;i++){
         j=n-i-1;
-        arr[i]=arr[j];
+        arr_two[i]=arr_one[j];
     }
     for(int i=0;i<n;i++){
-        cout<<arr[i];
+        cout<<arr_two[i];
     }
 }
