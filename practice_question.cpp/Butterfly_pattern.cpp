caf4d0662b50d82992b6279f5995103a7e6935ca
muskan <muskan8878h@gmail.com>
2024-06-29 11:45:19 +0530

    butterfly pattern printing

---
+++ practice_question.cpp/Butterfly_pattern.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<=n;j++){
+            if(j<=n-i){
+                cout<<"  ";
+            }
+            else{
+                cout<<"*";
+            }
+        }cout<<endl;
+    }
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<=n;j++){
+            if(i<j){
+                cout<<"*";
+            }
+            else{
+                cout<<"  ";
+            }
+        }cout<<endl;
+    }
+
+
+
+    // for(int i=0;i<=n;i++){
+    //     for(int j=0;j<=n;j++){
+    //         if(j<=n-i){
+    //             cout<<" ";
+    //         }
+    //         else{
+    //             cout<<"*";
+    //         }
+    //     }cout<<endl;
+    // }
+    
+    // for(int i=0;i<=n;i++){
+    //     for(int j=0;j<=n;j++){
+    //         if(i<j){
+    //             cout<<"*";
+    //         }
+    //         else{
+    //             cout<<" ";
+    //         }
+    //     }cout<<endl;
+    // }
+}
