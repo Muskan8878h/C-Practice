21cd921900f0d4481fc59f527df39f3870324282
muskan <muskan8878h@gmail.com>
2024-06-30 16:30:29 +0530

    diference between subarrays and subsequence

---
+++ array_.cpp/subarray_vs_subsequence.cpp
@@ -0,0 +1,7 @@
+// SUBARRAY - continuous part of the array.
+// number of subarray of an array with the element = nC2 + n = (n*(n+1))/2
+
+// SUBSEQUENCE - is a sequence thatcan be derived an array by electing zero or more elements, without changing the order of thr remaining elements.
+// number of subsequence of an array with the element = 2^n
+
+// everysubarray is a subsequence but every subsequence is not a subarray.
