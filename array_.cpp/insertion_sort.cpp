b34cf0391825bcc4698427f35af63ce6a8f482bf
muskan <muskan8878h@gmail.com>
2024-06-30 15:41:45 +0530

    explaination of insertion  sort with example

--- array_.cpp/insertion_sort.cpp
+++ array_.cpp/insertion_sort.cpp
@@ -16,9 +16,17 @@ int main(){
         }
         arr[j+1]=temp;
     }
-    
+
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
     return 0;
 }
+
+// explaination - insert an element from unsorted array to its correct position in sorted array
+// 12, 45, 23, 51, 19, 8 
+// 12, 45, 23, 51, 19, 8
+// 12, 23, 45, 51, 19, 8
+// 12, 23, 45, 51, 19, 8
+// 12, 19, 23, 45, 51, 8
+// 8, 12, 19, 23, 45, 51
