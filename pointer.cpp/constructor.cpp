bd160a3e935341a3e385bfa7e43fc614cfb1a5f5
muskan <muskan8878h@gmail.com>
2024-07-02 11:19:43 +0530

    parametric constructor

--- pointer.cpp/constructor.cpp
+++ pointer.cpp/constructor.cpp
@@ -6,19 +6,37 @@ class bank{
         double balance;
         string password;
     public:
-        bank(){
-            cout<<"hey!Welcome to SBI bank"<<endl;
-            bank_name = "SBI";//initialsise the construction 
+        //non parametrized constructor
+        // bank(){
+        //     cout<<"hey!Welcome to SBI bank"<<endl;
+        //     bank_name = "SBI";//initialsise the construction 
+        // }
+
+        //parametrized constructor
+        bank(string a, string u, string b){
+            accoNo=a;
+            username=u;
+            bank_name=b;
         }
         string accoNo;
         string username;
         string bank_name;
 
+        void getInfo(){
+            cout<<"accoNo. "<<accoNo<<endl;
+            cout<<"username "<<username<<endl;
+        }
+
 };
 int main(){
-    bank b1; //constructor call
-    b1.accoNo = "76447";
-    b1.username = "Rahul";
-    cout<<b1.bank_name<<endl;
-    cout<<b1.accoNo<<endl<<b1.username<<endl;
+    // non parametrized
+    // bank b1; //constructor call
+    // b1.accoNo = "76447";
+    // b1.username = "Rahul"; 
+    // cout<<b1.accoNo<<endl<<b1.username<<endl;
+
+    // parametrized
+    bank b1("76447","ram","SBI");
+    b1.getInfo();
+    
 }
