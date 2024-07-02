37478d23f54ebe38cb74c472562a5dcecadaf035
muskan <muskan8878h@gmail.com>
2024-07-02 10:19:49 +0530

    access the private value using function in main

--- OOPS/oops.cpp
+++ OOPS/oops.cpp
@@ -3,16 +3,27 @@
 using namespace std;
 class teacher{
     //propertices
-// private:
+private:
+    double salary;
 public:
     string name;
     string dept;
     string subject;
-    double salary;
+    
 
     //methods or member function 
     void changeDept(string newDept){
         dept = newDept;
+    }
+
+// we get the access of salary for some cases using function 
+    // setter is used to set the value of private members
+    void setSalary(double s){
+        salary = s;
+    }
+    // getter is used to get the value of private members
+    double getSalary(){
+        return salary;
     }
 };
 
@@ -21,8 +32,10 @@ int main(){
     t1.name = "Ram";
     t1.subject = "c++";
     t1.dept = "computer science";
-    t1.salary = 10000;
-    cout<<t1.name<<endl<<t1.dept<<endl<<t1.subject<<endl<<t1.salary<<endl;
+    // t1.salary = 10000;  //it will through error
 
+    t1.setSalary(25000);
+    cout<<t1.name<<endl<<t1.dept<<endl<<t1.subject<<endl;
+    cout<<t1.getSalary()<<endl;
     return 0;
 }
