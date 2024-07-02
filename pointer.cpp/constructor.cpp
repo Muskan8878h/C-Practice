a5b9066c0f7029e3a394a13fc3ca2dfc222b548f
muskan <muskan8878h@gmail.com>
2024-07-02 11:26:01 +0530

    use of this keyword

--- pointer.cpp/constructor.cpp
+++ pointer.cpp/constructor.cpp
@@ -6,6 +6,10 @@ class bank{
         double balance;
         string password;
     public:
+        string accoNo;
+        string username;
+        string bank_name;
+
         //non parametrized constructor
         // bank(){
         //     cout<<"hey!Welcome to SBI bank"<<endl;
@@ -13,18 +17,29 @@ class bank{
         // }
 
         //parametrized constructor
-        bank(string a, string u, string b){
-            accoNo=a;
-            username=u;
-            bank_name=b;
+        // bank(string a, string u, string b){
+        //     accoNo=a;
+        //     username=u;
+        //     bank_name=b;
+        // }
+        
+        // this (it is used if we want to give full name of parameters )
+        bank(string accoNo, string username, string bank_name){
+            // but complier get confused bcz both of same values 
+            // accoNo=accoNo;
+            // username=username;
+            // bank_name=bank_name;
+            // so we use this keyword 
+            this->accoNo=accoNo;
+            this->username=username;
+            this->bank_name=bank_name;
         }
-        string accoNo;
-        string username;
-        string bank_name;
+        
 
         void getInfo(){
             cout<<"accoNo. "<<accoNo<<endl;
             cout<<"username "<<username<<endl;
+            cout<<"bank_name "<<bank_name<<endl;
         }
 
 };
