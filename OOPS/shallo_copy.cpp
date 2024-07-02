9bbfc2d1d9d85b71edf42b900489a0b7d6943de3
muskan <muskan8878h@gmail.com>
2024-07-02 11:58:23 +0530

    copy constructor it all work on statically allocated memory till now

---
+++ OOPS/shallo_copy.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class student{
+
+    public:
+        string name;
+        double cgpa;
+    student(string name, double cgpa){
+        this->name = name;
+        this->cgpa = cgpa;
+    }
+
+    //copy constructor 
+    // student(student &obj){
+    //     this->name = obj.name;
+    //     this->cgpa = obj.cgpa;
+    // }
+    void getInfo(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"CGPA: "<<cgpa<<endl;
+    }
+};
+int main(){
+    student s1("ram",8.5);
+    student s2(s1);
+    s2.getInfo();
+
+
+}
