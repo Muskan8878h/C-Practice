fb6414d49f1c5710383b7ab90aef6fc61688cd8c
muskan <muskan8878h@gmail.com>
2024-07-13 15:19:17 +0530

    hashing

--- .100days.cpp/day_6.cpp/map.cpp
+++ .100days.cpp/day_6.cpp/map.cpp
@@ -13,6 +13,10 @@ int main(){
     for(int i=0;i<n;i++){
         mpp[arr[i]]++;
     }
+    // iterate in the map
+    // for(auto it: mpp){
+    //     cout<<it.first<<"->"<<it.second<<endl;
+    // }
     int q;
     cin>>q;
     while(q--){
