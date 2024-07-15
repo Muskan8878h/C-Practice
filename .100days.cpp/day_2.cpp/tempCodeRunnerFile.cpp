8b43ff5ba51ab2250125f9f1625963747d1e0261
muskan <muskan8878h@gmail.com>
2024-07-15 11:55:02 +0530

    try to solve the pattern question with another method

--- .100days.cpp/day_2.cpp/tempCodeRunnerFile.cpp
+++ .100days.cpp/day_2.cpp/tempCodeRunnerFile.cpp
@@ -1,13 +1,30 @@
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<i;j++){
-            cout<<char(65+i-1);
-        }
-        cout<<endl;
-    }
-}
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         for(int j=0;j<n-i;j++){
+//             cout<<"  ";
+//         }
+//         for(int j=n-i;j<n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     for(int i=1;i<=n;i++){
+//         for(int j=n;j>=i;j--){
+//             cout<<"*";
+//         }
+//         for(int j=1;j<=n;j++){
+//             if(i>j){
+//                 cout<<"  ";
+//             }
+//             else{
+//                 cout<<"*";
+//             }
+//         }cout<<endl;
+//     }
+// }  
