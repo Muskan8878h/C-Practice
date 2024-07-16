385f18fda555e04b916610dfa7e6278277a2a7cb
muskan <muskan8878h@gmail.com>
2024-07-16 09:36:49 +0530

    selection sort

--- .100days.cpp/day_8.cpp/selection_sort.cpp
+++ .100days.cpp/day_8.cpp/selection_sort.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 using namespace std;
 int selectionSort(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=i;j<n;j++){
-            if(arr[i]>arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+    // for(int i=0;i<n-1;i++){
+    //     for(int j=i;j<n;j++){
+    //         if(arr[i]>arr[j]){
+    //             int temp=arr[i];
+    //             arr[i]=arr[j];
+    //             arr[j]=temp;
+    //         }
+    //     }
+    // }
+
+    for(int i=0;i<=n-2;i++){
+        int mini=i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
             }
         }
+        int temp=arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
     }
 }
 int main(){
