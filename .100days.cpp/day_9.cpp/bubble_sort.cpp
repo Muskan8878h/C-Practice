878f6a7865459ec5ae65b4707be04a1dc7fbcf49
muskan <muskan8878h@gmail.com>
2024-07-18 10:05:42 +0530

    bubble sort algo

--- .100days.cpp/day_9.cpp/bubble_sort.cpp
+++ .100days.cpp/day_9.cpp/bubble_sort.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
-// int bubbleSort(int arr[], int n){
-//     for(int i=n-1;i>=0;i--){
-//         for(int j=0; j<=i-1; j++){
-//             if(arr[j]> arr[j+1]){
-//                 int temp = arr[j+1];
-//                 arr[j+1]=arr[j];
-//                 arr[j]=temp;
-//             }
-//         }
-//     }
-// }
+int bubbleSort(int arr[], int n){
+    for(int i=n-1;i>=0;i--){
+        for(int j=0; j<=i-1; j++){
+            if(arr[j]> arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
 int main(){
     int n;
     cin>>n;
@@ -18,7 +18,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    // bubbleSort(arr,n);
+    bubbleSort(arr,n);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
