e6a2762b53eba630dcfe406d880112f94b37deb4
muskan <muskan8878h@gmail.com>
2024-08-16 13:24:44 +0530

    multimap

---
+++ .100days.cpp/day_12_to_20/multimap.cpp
@@ -0,0 +1,10 @@
+// in map only unique keys exist 
+// but in multimap more than one key can exist 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    multimap<int,int> m;
+    // multimap are rarely used insted of multimap we use as following
+    map<int,vector<string>> m;
+}
