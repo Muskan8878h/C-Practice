7ee099967c1bf6ee0de5883868a440f08e3559fd
muskan <muskan8878h@gmail.com>
2024-08-22 10:38:26 +0530

    paramiterized constructor

--- OOPS/parameterizedORDefaultConstructor.cpp
+++ OOPS/parameterizedORDefaultConstructor.cpp
@@ -1,2 +1,29 @@
 #include<iostream>
 using namespace std;
+class complex{
+    int a,b;
+    public:
+    
+    complex(int ,int);
+    void printNumber(){
+        cout<<"Your no. is "<<a<<" + "<<b<<"i"<<endl;
+    }
+};
+complex :: complex(int x,int y) //this is parametrized constructor as it takes 2 parameters.
+{
+    a=x;
+    b=y;
+}
+int main(){
+    // implicit call
+    complex c(4,5); 
+
+    // explicit call
+    complex b=complex(5,7);
+
+    // complex c;
+    c.printNumber();
+    b.printNumber();
+    return 0;
+}
+
---
+++ OOPS/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+nt main(){
+    // implicit call
+    complex c(4,5); 
+
+    // explicit call
+    complex b=complex(5,7);
+
+    // complex c;
+    c.printNumber();
+    b.printNumber();
+    return 0;
+}
